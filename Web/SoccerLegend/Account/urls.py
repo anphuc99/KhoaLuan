a86d5d32340682467f7f1a164b8fc7f28c3bce7a
anphuc99 <86525411+anphuc99@users.noreply.github.com>
2022-12-31 15:02:30 +0700

    up web

--- Web/SoccerLegend/Account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("register", views.RegisterAPI.as_view(), name="index"),
-    path("login", views.LoginAPI.as_view(), name="login"),
-    path("token", views.TokenAPI.as_view(), name = "token")
-]
